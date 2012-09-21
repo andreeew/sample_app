b1787e30e0b63b4c9bb6346d1972c3954aa0e061
andreeew <andreeew@gmail.com>
2012-09-21 15:52:33 +0200

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home page" do
+    it "should have the content 'Sample_app'" do
+    	visit '/static_pages/home'
+    	page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "shoud have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title'), 
+                          :text => "Ruby on Rails Tutorial Sample App | Home"
+    end
+  end
+
+  describe "Help page" do
+  	it "should have the content 'Help'" do
+  		visit '/static_pages/help'
+  		page.should have_selector('h1', :text => 'Help')
+  	end
+
+    it "shoud have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title'), 
+                          :text => "Ruby on Rails Tutorial Sample App | Help"
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "shoud have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title'), 
+                          :text => "Ruby on Rails Tutorial Sample App | About Us"
+    end
+  end
+
+end
