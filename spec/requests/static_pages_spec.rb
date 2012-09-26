501b0ca04eebb16d717d5c4b0eaa2239c02d4e1a
andreeew <andreeew@gmail.com>
2012-09-26 22:08:28 +0200

    Setting test envoriment

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
-    it "should have the content 'Sample_app'" do
+    it "should have the content 'Sample app'" do
     	visit '/static_pages/home'
     	page.should have_selector('h1', :text => 'Sample App')
     end
@@ -11,7 +13,7 @@ describe "StaticPages" do
     it "shoud have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title'), 
-                          :text => "Ruby on Rails Tutorial Sample App | Home"
+                        :text => "#{base_title} | Home"
     end
   end
 
@@ -24,7 +26,7 @@ describe "StaticPages" do
     it "shoud have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title'), 
-                          :text => "Ruby on Rails Tutorial Sample App | Help"
+                        :text => "#{base_title} | Help"
     end
   end
 
@@ -37,7 +39,20 @@ describe "StaticPages" do
     it "shoud have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title'), 
-                          :text => "Ruby on Rails Tutorial Sample App | About Us"
+                        :text => "#{base_title} | About Us"
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title'),
+                        :text => "#{base_title} | Contact"
     end
   end
 
